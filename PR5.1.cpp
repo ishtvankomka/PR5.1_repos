3065c21d2ff98c4e5c615325105386c5a928a1db
ishtvankomka <you@example.com>
2021-10-18 12:38:00 +0300

    first master commit

---
+++ PR5.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double g(const double a, const double b); // прототип
+int main()
+{
+	double s, t;
+		cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (g(1, s) + (1 + g(t, 1) * g(t, 1)) * (1 + g(t, 1) * g(t, 1))) 
+		/ (1 + g(s + t, 1) * g(s + t, 1) * g(s + t, 1));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double g(const double a, const double b) // визначення
+{
+	return (a * a) + (a * b) + (b * b);
+}
